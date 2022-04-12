36b4776bf14dc1182a2abca88091691812c7d30a
CodingCas <casweeno2000@gmail.com>
2022-04-12 01:01:55 +0100

    ICO Init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x5C8f5D80FD653a885832dF5c1f09978C81A8cDC4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
